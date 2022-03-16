502bd650dfa86bec7ccc3cce00576416ae502de4
Jackie Dallas <jackie.r.dallas@gmail.com>
2022-03-16 00:22:53 -0500

    first commmit

---
+++ Sol-System-Dot-Net/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Sol_System_Dot_Net.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
